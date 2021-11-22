66f28ef50277710de236eb38cabf412f50b9dc0a
Nayunn <antoine.toffano@gmail.com>
2021-11-22 13:51:13 +0100

    syntax

--- roi_des_roses.py
+++ roi_des_roses.py
@@ -99,7 +99,7 @@ def demande_action(couleur, plateau, l_roi, c_roi, main):
         action = input()
         if action == "passe":
             return action
-        if action == "pioche" and len(main) < 5:
+        if action == "pioche" and len(main) < 5: # On vérifie que la main n'est pas pleine si le joueur choisit de piocher.
             return action
         if action in main_jouable(plateau, l_roi, c_roi, main):
             return action
@@ -125,40 +125,35 @@ def bouge_le_roi(plateau, l_roi, c_roi, main_r, main_b, defausse, carte, couleur
 ### Definition des territoires ###
 def territoire(plateau, ligne, colonne, couleur):
     territoire = []
-    if plateau[ligne][colonne] == couleur:
+    if plateau[ligne][colonne] == couleur: # Si la case est de la bonne couleur, on ajoute ses coordonnées à la liste des territoires.
         territoire.append([ligne, colonne])
-        territoire_voisins = check_voisins(plateau, ligne, colonne, couleur, [])
-        print("ter_vois1" + str(territoire_voisins))
-        while territoire_voisins != []:
-            for voisin in territoire_voisins:
-                if voisin not in territoire:
+        territoire_voisins = check_voisins(plateau, ligne, colonne, couleur) # On regarde si cette case possède des voisins de même couleur.
+        while territoire_voisins != []: # Tant qu'il ne reste pas de cases voisines de la même couleur (i.e que le bord du territoire n'a pas été atteint):
+            for voisin in territoire_voisins: # Pour chaque case de la même couleur voisine de notre case:
+                if voisin not in territoire: # On l'ajoute au territoire si elle n'y est pas déjà.
                     territoire.append(voisin)
-                    print("terri =" +str(territoire))
-                    print(voisin)
-                    new_line = voisin[0]
-                    new_col = voisin[1]
-                    territoire_voisins += check_voisins(plateau, new_line, new_col, couleur, territoire_voisins)
-                print("ter_vois" + str(territoire_voisins))
-                territoire_voisins.remove(voisin)
-        a_renvoyer = []
+                    territoire_voisins += check_voisins(plateau, voisin[0], voisin[1], couleur) # On vérifie si elle n'a pas des cases voisines de la même couleur.
+                    # Si oui, on les ajoute à la liste des voisins qui vont à leur tour (i.e à la prochaine itération) être scannés pour déterminer leurs cases adjacentes..
+                territoire_voisins.remove(voisin) # On supprime la case de la liste à analyser
+        a_renvoyer = [] # Les 3 lignes suivantes permettent de convertir le territoire d'un format liste de listes vers liste de tuples.
         for case in territoire:
             a_renvoyer.append(tuple(case))
         return a_renvoyer
+    return [] # Si la case d'origine n'est pas de la bonne couleur, on renvoie une liste vide.
 
-
-
-
-def check_voisins(plateau, ligne, colonne, couleur, territoire_voisins):
+# Fonction enregistrant les coordonnées des voisins nord, sud, est et ouest d'une case fournie si elles sont de la même couleur et renvoyant leur coordonnées.
+def check_voisins(plateau, ligne, colonne, couleur):
     voisins = []
-    if plateau[ligne +1] [colonne] == couleur and plateau[ligne +1] [colonne] not in territoire_voisins:
-        voisins.append([ligne +1, colonne])
-    if plateau[ligne -1] [colonne] == couleur:
+    if ligne + 1 < len(plateau) and plateau[ligne +1] [colonne] == couleur: # On vérifie ici si il existe bien une case au Sud de celle d'origine(i.e on est au bord du plateau), et si sa la couleur correspond.
+        voisins.append([ligne +1, colonne]) # Si oui, on l'ajoute à une liste qui sera retournée.
+    if ligne - 1 >= 0 and plateau[ligne -1] [colonne] == couleur:
         voisins.append([ligne -1, colonne])
-    if plateau[ligne] [colonne + 1] == couleur:
+    if colonne + 1 < len(plateau) and plateau[ligne] [colonne + 1] == couleur:
         voisins.append([ligne, colonne + 1])    
-    if plateau[ligne] [colonne - 1] == couleur:
+    if colonne - 1 >= 0 and plateau[ligne] [colonne - 1] == couleur:
         voisins.append([ligne, colonne - 1])
     return voisins
+plt = [["R" for count in range(9)] for count in range(9)]
 
 plt[2][5]="R"
 plt[2][6]="R"
@@ -170,13 +165,15 @@ plt[5][6]="R"
 plt[2][4]="R"
 plt[5][5]="R" 
 plt[1][5]="R"
-print(plt)
-afficher_jeu(plt , lr, cr, mr, mb)
-print(territoire(plt, 2, 4, "R"))
+plt[0][5]="R"
+
+
+print(len(territoire(plt, 2, 4, "R")))
+print(len(list(dict.fromkeys(territoire(plt, 2, 4, "R")))))
 
 ### Scores ###
 def score(plateau, couleur):
-    pass
+    
 
 
 ### Boucle de jeu principale ###
